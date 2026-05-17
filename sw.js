60861c215ce916146467fec6831dfb75dad551b5
Abdullah-Al Noman <81487006+thenomandev@users.noreply.github.com>
2026-05-18 01:59:53 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "tallykhata-v3"; // ভার্সন পরিবর্তন করা হলো ফোর্সবল আপডেটের জন্য
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./home.css",
+  "./ledger.css",
+  "./form.css",
+  "./app.js",
+  "./utils.js",
+  "./db.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
